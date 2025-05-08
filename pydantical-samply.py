ede9776e80e871d9e5a4b4bc7cacf769fa5de306
Oskcaj <oskcaj@gmail.com>
2025-05-08 23:53:20 +0800

    Create pydantical-samply.py

---
+++ pydantical-samply.py
@@ -0,0 +1,27 @@
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
+
+model = OpenAIModel(
+    'google/gemini-2.0-flash-lite-001',
+    provider=OpenAIProvider(
+        base_url='https://openrouter.ai/api/v1',
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        http_client=httpx.AsyncClient(verify=False)
+    ),
+)
+
+simple_agent = Agent(
+    model=model,
+    # 'Be concise, reply with one sentence.' is enough for some models (like openai) to use
+    # the below tools appropriately, but others like anthropic and gemini require a bit more direction.
+    system_prompt=(
+        'Be concise, reply with one sentence.'
+        'Use the `get_lat_lng` tool to get the latitude and longitude of the locations, '
+        'then use the `get_weather` tool to get the weather.'
+    ),
+    deps_type=Deps,
+    retries=2,
+    instrument=True,
+    
+)
