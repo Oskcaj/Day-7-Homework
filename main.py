bcb9f7a6360577a09afc8449c11e86faea51360d
Jack So <oskcaj@gmail.com>
2025-05-10 12:31:07 +0800

    8

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+import os
+import chainlit as cl
+from dotenv import load_dotenv
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
-from dotenv import load_dotenv
 #import httpx
-import os
-#import chainlit as cl
-
-load_dotenv() 
+load_dotenv()
 
 model = OpenAIModel(
     'google/gemini-2.0-flash-lite-001',
     provider=OpenAIProvider(
         base_url='https://openrouter.ai/api/v1',
         api_key=os.getenv("OPENROUTER_API_KEY"),
-       #http_client=httpx.AsyncClient(verify=False)
+        #http_client=httpx.AsyncClient(verify=False)
     ),
 )
 
@@ -24,11 +23,10 @@ simple_agent = Agent(
     ),   
 )
 
-result_sync = simple_agent.run_sync('What is the capital of Italy?')
-print(result_sync.output)
+#result_sync = simple_agent.run_sync('What is the capital of Italy?')
+#print(result_sync.output)
 
-
-#@cl.on_message
-#async def on_message(message: cl.Message):
-    #result = await simple_agent.run(message.content)
-    #await cl.Message(content=result.output).send()
+@cl.on_message
+async def on_message(message: cl.Message):
+    result = await simple_agent.run(message.content)
+    await cl.Message(content=result.output).send()
